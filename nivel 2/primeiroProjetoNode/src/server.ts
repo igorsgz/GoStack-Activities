9476f049eb49422830a4aa880efa9e4136344396
Igor S. Silvano <59940906+igorsvn@users.noreply.github.com>
2021-02-03 21:51:35 -0300

    integrando o banco e alguns ajustes;
    Integrando um banco pgsql usando typeorm e fazendo algumas alterações nas rotas;

--- nivel 2/primeiroProjetoNode/src/server.ts
+++ nivel 2/primeiroProjetoNode/src/server.ts
@@ -1,13 +1,14 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
 app.use(express.json()); // faz com que o express entenda requisições em formato JSON
-
 app.use(routes);
-
-app.get('/', (request, response) => response.json({ message: 'API Rodando!' }));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
